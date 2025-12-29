81935e57150965af2f7f408716ca017e4f5994f6
avAs <abas.zade@outlook.com>
2025-12-29 13:22:27 +0330

    feat: add final function without test

--- euler/23. Non Abundant Sums.py
+++ euler/23. Non Abundant Sums.py
@@ -14,13 +14,30 @@ def abundant(num: int):
 # print(abundant(12))
 
 
-def abundant_set(start: int = 1, end: int = 28124):
-    abundant_ls = set()
+def abundant_tuple(start: int = 1, end: int = 28124):
+    abundant_ls = []
     for i in range(start, end):
         if abundant(i):
-            abundant_ls.add(i)
-    return abundant_ls
+            abundant_ls.append(i)
+    return tuple(abundant_ls)
 
 
-n = abundant_set()
-print(n)
+# n = abundant_tuple()
+# print(n)
+
+
+def solve(num_set: set):
+    nums = {i: True for i in range(1, 28124)}
+    for i, num_1 in enumerate(num_set):
+        for num_2 in num_set[i:]:
+            sum = num_1 + num_2
+            if sum > 28123:
+                break
+            if nums[sum]:
+                nums[sum] = False
+    total = 0
+    for k, boo in nums.items():
+        if boo:
+            total += k
+
+    return total
